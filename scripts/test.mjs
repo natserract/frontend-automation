d7aa8503364779571552927d1331071ccee77f44
Alfin Surya <benjaminstwo@gmail.com>
2024-06-08 21:55:44 +0800

    update gh actions test configuration

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,7 +1,7 @@
 import { spawn } from 'node:child_process'
 
 console.log('Running ESLint, type checker, and unit tests...')
-spawn('pnpm', ['test:cy'], {
+spawn('pnpm', ['test:extended'], {
   shell: true,
   stdio: ['inherit', 'inherit', 'inherit'],
 })
